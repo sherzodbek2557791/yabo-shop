fa565a8f87612d2b516b5f40453c3d0a0e845fd9
Sherzodbek <leonbek2557791@gmail.com>
2019-05-17 18:38:26 +0500

    added template

--- yabo-shop-ui/src/main.js
+++ yabo-shop-ui/src/main.js
@@ -2,6 +2,24 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
 
+/**
+ * Styles imported
+ */
+import "./assets/css/animate.min.css";
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/config.css";
+import "./assets/css/font-awesome.min.css";
+import "./assets/css/owl.carousel.css";
+import "./assets/css/owl.transitions.css";
+import "./assets/css/style.css";
+import "./assets/css/colors/blue.css";
+import "./assets/css/colors/dark-green.css";
+import "./assets/css/colors/green.css";
+import "./assets/css/colors/navy.css";
+import "./assets/css/colors/orange.css";
+import "./assets/css/colors/red.css";
+
+
 import routes from "./routes";
 
 Vue.config.productionTip = false;
